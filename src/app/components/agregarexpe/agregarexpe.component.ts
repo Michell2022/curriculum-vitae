b20b365ca484eb2dfbb9acb416cf49f699e9392a
Michell2022 <sistemas.angel04@gmail.com>
2023-01-05 09:09:42 -0500

    modulos y componentes creados

---
+++ src/app/components/agregarexpe/agregarexpe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregarexpe',
+  templateUrl: './agregarexpe.component.html',
+  styleUrls: ['./agregarexpe.component.css']
+})
+export class AgregarexpeComponent {
+
+}
---
+++ src/app/components/components.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AgregarexpeComponent } from './agregarexpe/agregarexpe.component';
+
+
+
+@NgModule({
+  declarations: [
+    AgregarexpeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentsModule { }
