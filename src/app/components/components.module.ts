f6fd898d25f40cd197c764c6c4d64a93c72cf528
Michell2022 <sistemas.angel04@gmail.com>
2023-01-05 10:34:38 -0500

    haciendo deploy

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AgregarexpeComponent } from './agregarexpe/agregarexpe.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 
 
 @NgModule({
   declarations: [
-    AgregarexpeComponent
+    AgregarexpeComponent,
+    ContactoComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    AgregarexpeComponent
+    AgregarexpeComponent,
+    ContactoComponent
   ]
 })
 export class ComponentsModule { }
